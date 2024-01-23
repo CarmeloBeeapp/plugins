8d21ec9e229b68e740b8d74a367a028def502fa8
Carmelo <carmelo@beeapp.it>
2024-01-23 13:06:39 +0100

    Added groundOverlaysToAdd

--- packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/method_channel/method_channel_google_maps_flutter.dart
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/method_channel/method_channel_google_maps_flutter.dart
@@ -480,6 +480,7 @@ class MethodChannelGoogleMapsFlutter extends GoogleMapsFlutterPlatform {
       'polylinesToAdd': serializePolylineSet(polylines),
       'circlesToAdd': serializeCircleSet(circles),
       'tileOverlaysToAdd': serializeTileOverlaySet(tileOverlays),
+      'groundOverlaysToAdd': serializeGroundOverlaySet(tileOverlays),
     };
     if (defaultTargetPlatform == TargetPlatform.android) {
       return AndroidView(
